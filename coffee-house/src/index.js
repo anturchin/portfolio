5ea83e5c38690103ef04e3e46a8f70c1b3ab01eb
Andrey Turchin <tur4intwo@gmail.com>
2023-11-14 14:07:49 +0300

    feat: add babel-loader

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -1,7 +1,9 @@
 import './index.html';
 import './index.scss';
+import { slider } from './modules/slider';
 
 const sum = (a, b) => a + b;
 
 console.log(sum(5, 23));
 console.log(sum(55, 23));
+slider('i am slider').then(t => console.log(t));
--- coffee-house/webpack.config.js
+++ coffee-house/webpack.config.js
@@ -51,6 +51,18 @@ module.exports = {
 					'sass-loader'
 				],
 			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: "defaults" }]
+						]
+					}
+				}
+			},
 		]
 	},
 }
