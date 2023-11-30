a9cfff0bd313e115978c89c9d34a683bb899ce82
Andrey Turchin <tur4intwo@gmail.com>
2023-11-30 13:55:14 +0300

    feat: add events mouse and touch in the slider

--- coffee-house/src/modules/slider.js
+++ coffee-house/src/modules/slider.js
@@ -4,7 +4,7 @@ export const slider = () => {
 	const slidesWrapper = document.querySelector('.slides__wrapper');
 	const slidesInner = document.querySelector('.slides__inner');
 	const slidesField = document.querySelector('.slides__coffee');
-	const slides = document.querySelectorAll('.slides__item');
+	const slides = document.querySelectorAll('.slides__item');	
 	const prev = document.querySelector('.prev');
 	const next = document.querySelector('.next');
 	const lines = document.querySelectorAll('.lines__item');
@@ -14,6 +14,10 @@ export const slider = () => {
 	let slideIndex = 1;
 	let offset = 0;
 	let paused = false;
+
+	let isDragging = false;
+	let dragStartX = 0;
+	let dragDiff = 0;
 
 	slidesField.style.width = 100 * slides.length + '%';
 
@@ -69,20 +73,99 @@ export const slider = () => {
 	const activateInterval = () => {
 		paused = setInterval(() => {
 			nextSlide();
-		}, 5000);
+		}, 3000);
 	}
 
 	activateInterval();
 
+	const onPointerDown = (e) => {
+
+		isDragging = true;
+		dragStartX = e.clientX || e.touches[0].clientX;
+
+	}
+
+	const onPointerMove = (e) => {
+
+		if (!isDragging) return;
+
+		const currentX = e.clientX || e.touches[0].clientX;
+		dragDiff = dragStartX - currentX;
+
+		slidesField.style.transition = 'none';
+		slidesField.style.transform = `translateX(${-offset - dragDiff}px)`;
+
+	}
+
+	const onPointerUp = () => {
+
+		if (!isDragging) return;
+
+		isDragging = false;
+		slidesField.style.transition = '';
+
+		if (dragDiff > 50) {
+			nextSlide();
+		}
+		if (dragDiff < -50) {
+			prevSlide();
+		}
+
+		dragDiff = 0;
+
+	}
+
+	const onPointerLeave = () => {
+
+		if (isDragging) {
+			isDragging = false;
+			slidesField.style.transition = '';
+			slidesField.style.transform = `translateX(${-offset}px)`;
+		}
+
+	}
+
 	next.addEventListener('click', nextSlide);
 	prev.addEventListener('click', prevSlide);
 
-	slidesWrapper.addEventListener('mouseenter', () => {
+	slidesWrapper.addEventListener('pointerenter', () => {
 		clearInterval(paused);
 	})
-	
-	slidesWrapper.addEventListener('mouseleave', () => {
+
+	slidesWrapper.addEventListener('pointerleave', () => {
 		activateInterval();
+	});
+
+	slidesInner.addEventListener('mousedown', (e) => {
+		onPointerDown(e)
 	})
+
+	slidesInner.addEventListener('mousemove', (e) => {
+		onPointerMove(e);
+	})
+
+	slidesInner.addEventListener('mouseup', () => {
+		onPointerUp();
+	});
+
+	slidesInner.addEventListener('mouseleave', () => {
+		onPointerLeave();
+	});
+
+	slidesInner.addEventListener('touchstart', (e) => {
+		onPointerDown(e)
+	})
+
+	slidesInner.addEventListener('touchmove', (e) => {
+		onPointerMove(e);
+	})
+
+	slidesInner.addEventListener('touchend', () => {
+		onPointerUp();
+	});
+
+	slidesInner.addEventListener('touchcancel', () => {
+		onPointerLeave();
+	});
 
 }
