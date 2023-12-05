ab98b668361de8f05e0efe3c2872f21e349f63f3
Andrey Turchin <tur4intwo@gmail.com>
2023-12-05 19:05:25 +0300

    refactor: change calculate sum

--- coffee-house/src/modules/components/modals/calculateForm.js
+++ coffee-house/src/modules/components/modals/calculateForm.js
@@ -4,62 +4,80 @@ export const handleCalcForm = (products) => {
 	const inputRadio = document.querySelectorAll('[type="radio"]');
 	const inputCheckbox = document.querySelectorAll('[type="checkbox"]');
 
-	let sum = 0;
+	let sum;
 
-	const updateSum = (oldSum) => {
-		
-		let totalPrice = document.querySelector('.modal__price');
-		let total = +totalPrice.textContent.slice(1);
+	const updateSum = (sum) => {
+		const totalPrice = document.querySelector('.modal__price');
 		totalPrice.innerHTML = '';
-		total = total - oldSum + sum;
-		totalPrice.innerHTML = `$${total.toFixed(2)}`;
-
+		totalPrice.innerHTML = `$${sum.toFixed(2)}`;
 	}
 
-	const calcSum = (input) => {
-
-		let select;
-
-		if (input.parentElement.parentElement.getAttribute('data-sizes')) {
-			select = input.parentElement.parentElement.getAttribute('data-sizes')
-		}
-
-		if (input.parentElement.parentElement.getAttribute('data-additives')) {
-			select = input.parentElement.parentElement.getAttribute('data-additives')
-		}
-
-		let oldSum = sum;
-
-		if (input.type === 'radio') {
-			sum = 0;
-		}
-
+	const calcSum = (input)=> {
 		
 		const inputID = +input.parentElement.getAttribute('data-product-id');
 		const prodItem = products.find(item => item.id === inputID);
 
-		Object.entries(prodItem[select]).forEach(item => {
-			if (item[1][input.name] == input.value) {
-				if (input.checked) {
-					sum += +item[1]['add-price'];
-				} else {
-					sum -= +item[1]['add-price'];
+		const prodPrice = +prodItem.price;
+		const prodSizeS = prodItem.sizes.s;
+		const prodSizeM = prodItem.sizes.m;
+		const prodSizeL = prodItem.sizes.l;
+		
+		inputRadio.forEach(input => {
+			
+			if(input.checked){
+
+				switch (input.value) {
+					case prodSizeS.size:
+						sum += +prodSizeS['add-price'];
+						break;
+					case prodSizeM.size:
+						sum += +prodSizeM['add-price'];
+						break;
+					case prodSizeL.size:
+						sum += +prodSizeL['add-price'];
+						break;
+					default:
+						sum = 0;
 				}
+
 			}
-		});
+		})
+		
+		const prodAditOne = prodItem.additives[0];
+		const prodAditTwo = prodItem.additives[1];
+		const prodAditThree = prodItem.additives[2];
 
-		updateSum(oldSum);
 
+		inputCheckbox.forEach(input => {
+			
+			if(input.checked){
+
+				switch (input.value) {
+					case prodAditOne.name:
+						sum += +prodAditOne['add-price'];
+						break;
+					case prodAditTwo.name:
+						sum += +prodAditTwo['add-price'];
+						break;
+					case prodAditThree.name:
+						sum += +prodAditThree['add-price'];
+						break;
+					default:
+						sum = 0;
+				}
+
+			}
+		})
+
+		updateSum(sum + prodPrice);
 	}
-
 
 	function handleInputChange(inputs) {
 
 		inputs.forEach((input, i) => {
 			input.addEventListener('change', () => {
-
+				sum = 0;
 				calcSum(input);
-
 			})
 		})
 	}
