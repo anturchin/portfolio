71121b3576603f0cd74ab297fa96c60f476d0699
Andrey Turchin <tur4intwo@gmail.com>
2024-01-08 22:20:31 +0300

    feat: add array of questions for game and letters for the keyboard

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,2 +1,4 @@
 import "./index.html";
 import "./index.scss";
+
+import game from "./js/game";
---
+++ hangman/src/js/game.js
@@ -0,0 +1,5 @@
+import keys from "./keys";
+import questions from "./questions";
+
+console.log(keys);
+console.log(questions);
