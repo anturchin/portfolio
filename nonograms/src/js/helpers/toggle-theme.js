ae7b3224aca6466d01f5122ace37d4f5bdda7766
Andrey Turchin <tur4intwo@gmail.com>
2024-01-24 20:55:53 +0300

    feat: add level scss and update styles

--- nonograms/src/js/helpers/toggle-theme.js
+++ nonograms/src/js/helpers/toggle-theme.js
@@ -2,6 +2,7 @@ export const toggleTheme = (e) => {
   const target = e.target;
   const body = document.querySelector("body");
   const gameCell = document.querySelectorAll(".game__cell");
+  const gameLevel = document.querySelectorAll(".level__item");
 
   if (target.classList.contains("dark__button")) {
     target.classList.remove("dark__button");
@@ -26,6 +27,16 @@ export const toggleTheme = (e) => {
     } else {
       cell.classList.remove("light__cell");
       cell.classList.add("dark__cell");
+    }
+  });
+
+  gameLevel.forEach((level) => {
+    if (level.classList.contains("dark__button")) {
+      level.classList.remove("dark__button");
+      level.classList.add("light__button");
+    } else {
+      level.classList.remove("light__button");
+      level.classList.add("dark__button");
     }
   });
 };
