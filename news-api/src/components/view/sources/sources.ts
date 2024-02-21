818143f017d728e6e69f6f8fadd74ddaacf1641e
anturchin <tur4intwo@gmail.com>
2024-02-21 14:49:22 +0300

    feat: add types to sources

---
+++ news-api/src/components/view/sources/sources.ts
@@ -0,0 +1,32 @@
+import './sources.scss';
+import { ISource, INews } from '../../../types';
+
+class Sources implements INews<ISource> {
+    draw(data: ISource[]): void {
+        const fragment = document.createDocumentFragment() as DocumentFragment;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+            const sourceItemName = sourceClone.querySelector('.source__item-name') as HTMLElement;
+            if (sourceItemName) {
+                sourceItemName.textContent = item.name;
+            }
+
+            const sourceItem = sourceClone.querySelector('.source__item') as HTMLElement;
+            if (sourceItem) {
+                sourceItem.setAttribute('data-source-id', item.id);
+            }
+
+            fragment.append(sourceClone);
+        });
+
+        const sources = document.querySelector('.sources') as HTMLElement;
+        if (sources) {
+            sources.append(fragment);
+        }
+    }
+}
+
+export default Sources;
