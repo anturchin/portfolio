2118cf36bd285cfc33d575d5a5be749e3491d13b
anturchin <tur4intwo@gmail.com>
2024-02-21 19:04:37 +0300

    feat: add types to controller

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
 import AppLoader from './appLoader';
+import { ICallback } from '../../types';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: ICallback) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,20 +11,22 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: MouseEvent, callback: ICallback) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
+                    if (sourceId) {
+                        newsContainer.setAttribute('data-source', sourceId);
+                    }
                     super.getResp(
                         {
                             endpoint: 'everything',
                             options: {
-                                sources: sourceId,
+                                sources: sourceId ? sourceId : '',
                             },
                         },
                         callback
@@ -31,7 +34,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
