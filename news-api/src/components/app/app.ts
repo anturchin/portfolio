b18fb3caead5674f86b7cbf656cb2a44f7c2f014
anturchin <tur4intwo@gmail.com>
2024-02-21 20:25:52 +0300

    feat: add types to app

---
+++ news-api/src/components/app/app.ts
@@ -0,0 +1,33 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { Sources } from '../../types';
+
+class App {
+    private controller: AppController;
+    private view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        const sources = document.querySelector('.sources') as HTMLElement;
+        if (sources) {
+            sources.addEventListener('click', (e) => {
+                this.controller.getNews(e, (data) => {
+                    if (data) {
+                        this.view.drawNews(data);
+                    }
+                });
+            });
+        }
+        this.controller.getSources((data) => {
+            if (data) {
+                const sourceData = data as Sources;
+                this.view.drawSources(sourceData);
+            }
+        });
+    }
+}
+
+export default App;
