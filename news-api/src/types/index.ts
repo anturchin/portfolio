9dd6b7703e6f07bd3661decfb3889e8603885c3a
anturchin <tur4intwo@gmail.com>
2024-02-21 11:49:25 +0300

    feat: add types

---
+++ news-api/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface ISources {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface IResponse {
+    status: string;
+    sources: ISources[];
+}
