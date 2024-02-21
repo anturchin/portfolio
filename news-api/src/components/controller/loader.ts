f5f076e2f4b39118aa9f2f44b8fbd6e01bb5d2f5
anturchin <tur4intwo@gmail.com>
2024-02-21 17:26:17 +0300

    feat: add types to loader

---
+++ news-api/src/components/controller/loader.ts
@@ -0,0 +1,53 @@
+import { IOptions, TypeResp, IResponse, ICallback } from '../../types';
+
+class Loader {
+    private baseLink: string;
+    private options: IOptions;
+
+    constructor(baseLink: string, options: IOptions) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options = {} }: TypeResp,
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ): void {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    async errorHandler(res: Response): Promise<IResponse> {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+        const data = await res.json();
+        return data as IResponse;
+    }
+
+    makeUrl(options: IOptions, endpoint: string): string {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    async load(method: string, endpoint: string, callback: ICallback, options: IOptions = {}): Promise<void> {
+        try {
+            const response = await fetch(this.makeUrl(options, endpoint), { method });
+            const responseData = await this.errorHandler(response);
+            callback(responseData);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default Loader;
