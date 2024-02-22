dc4bb63d796824ef50fb2593cba8d1aca64b1651
anturchin <tur4intwo@gmail.com>
2024-02-22 13:47:41 +0300

    feat: update webpack config

--- news-api/config/build/buildLoaders.ts
+++ news-api/config/build/buildLoaders.ts
@@ -4,6 +4,22 @@ import { IBuildOptions } from './types/types';
 
 export const buildLoaders = ({ mode }: IBuildOptions): ModuleOptions['rules'] => {
     const isDev = mode === 'development';
+
+    const fontsLoader = {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'fonts/[name].[ext]',
+        },
+    };
+
+    const assetLoader = {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'images/[name].[ext]',
+        },
+    };
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
@@ -17,5 +33,5 @@ export const buildLoaders = ({ mode }: IBuildOptions): ModuleOptions['rules'] =>
         test: /\.html$/i,
         loader: 'html-loader',
     };
-    return [scssLoader, tsLoader, htmlLoader];
+    return [scssLoader, tsLoader, htmlLoader, assetLoader, fontsLoader];
 };
---
+++ news-api/src/global.d.ts
@@ -0,0 +1,13 @@
+declare module '*.module.scss' {
+    interface IClassNames {
+        [className: string]: string;
+    }
+    const className: IClassNames;
+    export = className;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
