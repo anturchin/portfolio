ef2547964753c6781f7b0fffc69918fb11706907
anturchin <tur4intwo@gmail.com>
2024-04-24 14:04:05 +0300

    feat: add logo

--- fun-chat/frontend/src/components/view/chat/footer/school/School.ts
+++ fun-chat/frontend/src/components/view/chat/footer/school/School.ts
@@ -1,15 +1,20 @@
 import { View } from '../../../View';
 
+import logo from '../../../../../assets/image/rsschool.svg';
+
 import './School.scss';
 
 export class School extends View {
     constructor() {
-        super({ tag: 'a', classNames: ['footer__school'], textContent: 'RSSchool' });
+        super({ tag: 'a', classNames: ['footer__school'] });
         this.setupSchool();
     }
 
     private setupSchool(): void {
         (this.getElement() as HTMLLinkElement).href = 'https://rs.school/';
         (this.getElement() as HTMLLinkElement).target = '_blanc';
+        const img = document.createElement('img');
+        img.src = logo;
+        this.addInnerElement(img);
     }
 }
